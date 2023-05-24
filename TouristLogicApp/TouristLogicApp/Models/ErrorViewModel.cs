79469fe1268275d499119d58b0ca1f9cf3aab3e0
NVenu <venu.nelapatla@ntgfreight.com>
2023-05-24 13:57:16 +0530

    initial commit

---
+++ TouristLogicApp/TouristLogicApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TouristLogicApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
